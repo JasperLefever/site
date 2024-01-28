2e454289a23a12d4f560d8aadc24b0e065e9658f
JasperLefever <jasper.lefever@telenet.be>
2024-01-28 12:58:34 +0100

    added UnderConstruction page and added browserrouter

---
+++ src/UnderConstruction.jsx
@@ -0,0 +1,12 @@
+function UnderConstruction() {
+    return (
+        <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold mt-8 mb-4">Under Construction</h1>
+            <p className="text-lg text-gray-700">
+                Sorry, this page is under construction. Please check back later for updates.
+            </p>
+        </div>
+    );
+}
+
+export default UnderConstruction;
